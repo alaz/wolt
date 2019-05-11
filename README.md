d9a1fbafa17809eee7762f44fa017ee0a3d82992
Alexander Azarov <self@alaz.me>
2019-05-11 20:49:46 +0300

    🚧 ✅ perform integration testing (WIP)

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Home assignment project
 
-## Run
+## Building
 
 ```
-mvn clean test exec:java
+mvn compile
 ```
 
-The main entry point will be http://localhost:8000/venue/human/hours , it accepts JSON and replies with `text/plain` per the task description.
+## Testing
+
+* Unit tests: `mvn test`
+* Integration tests: `mvn test -P it`
+
+## Running
+
+```
+mvn exec:java
+```
+
+The main entry point will be http://localhost:8000/venue/hours/human (which should be interpreted as "give me human represenation of a venue's open hours"). This endpoint accepts JSON and replies with `text/plain` per the task description.
--- src/test/scala/integration/ApiTest.scala
+++ src/test/scala/integration/ApiTest.scala
@@ -3,20 +3,54 @@ package me.alaz.interview.wolt.integration
 import java.io.File
 import com.datasift.dropwizard.scala.test.{ApplicationTest, BeforeAndAfterAllMulti}
 import com.google.common.io.Resources
+import javax.ws.rs.client.Entity
+import javax.ws.rs.core.MediaType
 import me.alaz.interview.wolt.Application
+import me.alaz.interview.wolt.api.WeekSchedule
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSpec, Matchers}
 
 @RunWith(classOf[JUnitRunner])
 class ApiTest extends FunSpec with BeforeAndAfterAllMulti with Matchers {
-    val app = ApplicationTest(this, new File(Resources.getResource("test-app.yml").toURI).getAbsolutePath) {
-      Application
-    }
+  val configFile = new File(Resources.getResource("test-app.yml").toURI).getAbsolutePath
+  val app = ApplicationTest(this, configFile)(Application)
+  lazy val client = app.newClient("test client")
+
+  def req(target: String) = for {
+    client <- client
+    server <- app.server
+  } yield {
+    client.target(server.getURI.resolve(target))
+  }
 
   describe("API") {
     it("responds with plain text") {
-      // TODO
+      for {r <- req("/venue/hours/human")} {
+        val response = r
+          .request(MediaType.TEXT_PLAIN)
+          .put(Entity.json("{}"))
+        response.getStatus should equal(200)
+        response.getMediaType.toString should equal(MediaType.TEXT_PLAIN)
+      }
+    }
+
+    it("rejects wrong `Accept`") {
+      for {r <- req("/venue/hours/human")} {
+        val response = r
+          .request(MediaType.TEXT_HTML)
+          .put(Entity.json("{}"))
+        response.getStatus should equal(406)
+      }
+    }
+
+    it("rejects wrong request type") {
+      for {r <- req("/venue/hours/human")} {
+        val response = r
+          .request(MediaType.TEXT_PLAIN)
+          .put(Entity.text("{}"))
+        response.getStatus should equal(415)
+      }
     }
   }
 }
